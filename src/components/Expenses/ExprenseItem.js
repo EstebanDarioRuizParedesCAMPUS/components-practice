4b43c8c2086c8b9495e044facf4ff362ecdc3d02
EstebanDarioRuizParedesCAMPUS <edrp.campus15@gmail.com>
2023-08-27 18:20:10 -0500

    girignore node_modules

--- src/components/Expenses/ExprenseItem.js
+++ src/components/Expenses/ExprenseItem.js
@@ -1,14 +1,29 @@
+import React, { useState } from "react"
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExprenseItem.css";
 
 const ExprenseItem = ({ date, title, amount }) => {
+
+  const [titlle,sertTittlle] = useState(title)
+
+  const clickHandler = ()=>{
+    if(titlle == title){
+      sertTittlle('Updatedd')
+    } else if (titlle == 'Updatedd'){
+      sertTittlle(title)
+    }
+    console.log(titlle);
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{titlle}</h2>
         <div className="expense-item__price">${amount}</div>
+        <button onClick={clickHandler}>Change title</button>
       </div>
     </Card>
   );
