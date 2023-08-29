f70f82d36d6da90a92c95be06ef6cd01baca2f0d
EstebanDarioRuizParedesCAMPUS <edrp.campus15@gmail.com>
2023-08-29 07:47:43 -0500

    filter Expense

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,21 +1,42 @@
 import { useState } from "react";
 import ExprenseItem from "./ExprenseItem";
 import Card from "../UI/Card";
-import ExpenseFilter from "./ExpenseFilter"
+import ExpenseFilter from "./ExpenseFilter";
 
 import "./Expenses.css";
- 
+
 const Expenses = (props) => {
-  const [ filteredYear, setFilteredYear ] = useState('2020')
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  let filterInfoText
+
+  if (selectedYear === '2019'){
+    filterInfoText = "2020, 2021, 2022 & 2023"
+  } else if (selectedYear === '2020') {
+    filterInfoText = "2019, 2021, 2022 & 2023"
+  } else if (selectedYear === '2021') {
+    filterInfoText = "2019, 2020, 2022 & 2023"
+  } else if (selectedYear === '2022') {
+    filterInfoText = "2019, 2020, 2021 & 2023"
+  } else if (selectedYear === '2023') {
+    filterInfoText = "2019, 2020, 2021 & 2022"
+  } else {
+    filterInfoText = "No one Year"
+  }
 
   const filterChangeHandler = (selectedYear) => {
-    setFilteredYear(selectedYear)
-  }
+    setFilteredYear(selectedYear);
+
+  };
 
   return (
     <Card className="expenses">
-      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
 
+      <p>Data for years {filterInfoText} is hidden</p>
 
       <ExprenseItem
         title={props.item[0].title}
@@ -39,6 +60,6 @@ const Expenses = (props) => {
       />
     </Card>
   );
-}
+};
 
 export default Expenses;
